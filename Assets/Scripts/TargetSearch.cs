bebaabbfffcf34a0ef30350e395cd03c298c1885
Ryunoske <dryunya94@bk.ru>
2023-11-14 23:05:37 +0300

    rearrange

---
+++ Assets/Scripts/TargetSearch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSearch : MonoBehaviour
+{
+    private RaycastHit[] hits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       //hits = Physics.RaycastAll(transform.position, )
+    }
+}
